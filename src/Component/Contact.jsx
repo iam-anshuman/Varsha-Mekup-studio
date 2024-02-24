1809ef1d21c5d1151068bbd83d7204b260987203
Anshuman Tiwari <118616967+iam-anshuman@users.noreply.github.com>
2024-02-25 02:35:44 +0530

    Add loading state to contact button

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -203,12 +203,22 @@ export default function Contact() {
                 value={userContact.message}
               ></textarea>
             </div>
+{
+isLoading ?
+              <button
+              className="text-white bg-indigo-300 border-0 py-2 px-6 focus:outline-none rounded text-lg"
+              disabled
+              >
+              Button
+              </button>
+              :            
             <button
               className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
               onClick={handleUserContacted}
             >
               Button
             </button>
+}
           </div>
         </div>
       </section>
